d5fd4931f75f0ae2f1ddd6e8d6edeb7bac85d3a9
peploleum <peploleum@gmail.com>
2018-12-18 11:24:21 +0100

    raw data dto

---
+++ src/main/java/com/peploleum/insight/yummy/dto/RawDataDTO.java
@@ -0,0 +1,203 @@
+package com.peploleum.insight.yummy.dto;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * A DTO for the RawData entity.
+ */
+public class RawDataDTO implements Serializable {
+
+    private String id;
+
+    @NotNull
+    private String rawDataName;
+
+    private String rawDataType;
+
+    private String rawDataSubType;
+
+    private String rawDataSourceName;
+
+    private String rawDataSourceUri;
+
+    private String rawDataSourceType;
+
+    private String rawDataContent;
+
+    private LocalDate rawDataCreationDate;
+
+    private LocalDate rawDataExtractedDate;
+
+    private String rawDataSymbol;
+
+    private byte[] rawDataData;
+    private String rawDataDataContentType;
+
+    private String rawDataCoordinates;
+
+    private String rawDataAnnotations;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRawDataName() {
+        return rawDataName;
+    }
+
+    public void setRawDataName(String rawDataName) {
+        this.rawDataName = rawDataName;
+    }
+
+    public String getRawDataType() {
+        return rawDataType;
+    }
+
+    public void setRawDataType(String rawDataType) {
+        this.rawDataType = rawDataType;
+    }
+
+    public String getRawDataSubType() {
+        return rawDataSubType;
+    }
+
+    public void setRawDataSubType(String rawDataSubType) {
+        this.rawDataSubType = rawDataSubType;
+    }
+
+    public String getRawDataSourceName() {
+        return rawDataSourceName;
+    }
+
+    public void setRawDataSourceName(String rawDataSourceName) {
+        this.rawDataSourceName = rawDataSourceName;
+    }
+
+    public String getRawDataSourceUri() {
+        return rawDataSourceUri;
+    }
+
+    public void setRawDataSourceUri(String rawDataSourceUri) {
+        this.rawDataSourceUri = rawDataSourceUri;
+    }
+
+    public String getRawDataSourceType() {
+        return rawDataSourceType;
+    }
+
+    public void setRawDataSourceType(String rawDataSourceType) {
+        this.rawDataSourceType = rawDataSourceType;
+    }
+
+    public String getRawDataContent() {
+        return rawDataContent;
+    }
+
+    public void setRawDataContent(String rawDataContent) {
+        this.rawDataContent = rawDataContent;
+    }
+
+    public LocalDate getRawDataCreationDate() {
+        return rawDataCreationDate;
+    }
+
+    public void setRawDataCreationDate(LocalDate rawDataCreationDate) {
+        this.rawDataCreationDate = rawDataCreationDate;
+    }
+
+    public LocalDate getRawDataExtractedDate() {
+        return rawDataExtractedDate;
+    }
+
+    public void setRawDataExtractedDate(LocalDate rawDataExtractedDate) {
+        this.rawDataExtractedDate = rawDataExtractedDate;
+    }
+
+    public String getRawDataSymbol() {
+        return rawDataSymbol;
+    }
+
+    public void setRawDataSymbol(String rawDataSymbol) {
+        this.rawDataSymbol = rawDataSymbol;
+    }
+
+    public byte[] getRawDataData() {
+        return rawDataData;
+    }
+
+    public void setRawDataData(byte[] rawDataData) {
+        this.rawDataData = rawDataData;
+    }
+
+    public String getRawDataDataContentType() {
+        return rawDataDataContentType;
+    }
+
+    public void setRawDataDataContentType(String rawDataDataContentType) {
+        this.rawDataDataContentType = rawDataDataContentType;
+    }
+
+    public String getRawDataCoordinates() {
+        return rawDataCoordinates;
+    }
+
+    public void setRawDataCoordinates(String rawDataCoordinates) {
+        this.rawDataCoordinates = rawDataCoordinates;
+    }
+
+    public String getRawDataAnnotations() {
+        return rawDataAnnotations;
+    }
+
+    public void setRawDataAnnotations(String rawDataAnnotations) {
+        this.rawDataAnnotations = rawDataAnnotations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        RawDataDTO rawDataDTO = (RawDataDTO) o;
+        if (rawDataDTO.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), rawDataDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "RawDataDTO{" +
+            "id=" + getId() +
+            ", rawDataName='" + getRawDataName() + "'" +
+            ", rawDataType='" + getRawDataType() + "'" +
+            ", rawDataSubType='" + getRawDataSubType() + "'" +
+            ", rawDataSourceName='" + getRawDataSourceName() + "'" +
+            ", rawDataSourceUri='" + getRawDataSourceUri() + "'" +
+            ", rawDataSourceType='" + getRawDataSourceType() + "'" +
+            ", rawDataContent='" + getRawDataContent() + "'" +
+            ", rawDataCreationDate='" + getRawDataCreationDate() + "'" +
+            ", rawDataExtractedDate='" + getRawDataExtractedDate() + "'" +
+            ", rawDataSymbol='" + getRawDataSymbol() + "'" +
+            ", rawDataData='" + getRawDataData() + "'" +
+            ", rawDataCoordinates='" + getRawDataCoordinates() + "'" +
+            ", rawDataAnnotations='" + getRawDataAnnotations() + "'" +
+            "}";
+    }
+}
