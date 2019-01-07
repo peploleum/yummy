9e6ef42bdac6ef37aff76537dc84a22523119ac8
peploleum <peploleum@gmail.com>
2019-01-07 17:25:52 +0100

    refactoring

--- src/main/java/com/peploleum/insight/yummy/config/RawDataSink.java
+++ src/main/java/com/peploleum/insight/yummy/config/RawDataSink.java
@@ -1,7 +1,6 @@
 package com.peploleum.insight.yummy.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.peploleum.insight.yummy.dto.SourceMessage;
 import com.peploleum.insight.yummy.dto.source.RssSourceMessage;
 import com.peploleum.insight.yummy.service.NerClient;
 import org.slf4j.Logger;
@@ -15,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import java.io.IOException;
 
 @Configuration
-// @EnableBinding(value = {Sink.class})
+@EnableBinding(value = {Sink.class})
 public class RawDataSink {
     private final Logger log = LoggerFactory.getLogger(RawDataSink.class);
 
@@ -31,17 +30,14 @@ public class RawDataSink {
     @Value("${ner}")
     private boolean ner;
 
-    //@StreamListener(Sink.INPUT)
+    @StreamListener(Sink.INPUT)
     public void handle(String message) {
         try {
-            ObjectMapper mapperObj = new ObjectMapper();
-
-            RssSourceMessage mess = mapperObj.readValue(message, RssSourceMessage.class);
+            final ObjectMapper mapperObj = new ObjectMapper();
+            final RssSourceMessage mess = mapperObj.readValue(message, RssSourceMessage.class);
             final String display = "Received: " + message;
             log.info(display);
             new NerClient().doSend(mess, urlner, urlinsight, ner);
-
-
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
