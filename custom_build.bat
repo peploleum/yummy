1eec3f0ef96315e91bc40e6510f894984a5c288a
peploleum <peploleum@gmail.com>
2019-01-21 13:45:15 +0100

    fixed custom build to push 1.0.0

--- custom_build.bat
+++ custom_build.bat
@@ -1,3 +1,4 @@
 call mvn clean package -DskipTests dockerfile:build
-docker tag yummy peploleum/yummy:latest
-docker push peploleum/yummy:latest
+docker tag yummy peploleum/yummy:1.0.0
+docker push peploleum/yummy:1.0
+.0
